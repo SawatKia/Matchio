35cbb99858f74bbb8230d6d834fd6fffb8a00bd3
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2025-05-01 15:55:23 +0700

    move constants to configs file

--- src/main.py
+++ src/main.py
@@ -5,101 +5,20 @@ from typing import Optional, Tuple, Dict
 import time
 import os # Import os for path joining
 
-from utils import initialize_logging, get_logger, FileManager
+from utils import initialize_logging, get_logger, FileManager, CONFIG, EXPECTED_COLUMN_MAPPINGS
 from processors import ReportProcess, Reports, TransactionMatcher 
-from gui import ApplicationGUI 
+# from gui import ApplicationGUI 
 
 # Get the pre-configured logger
 logger = get_logger()
 
-CONFIG = {
-    'csv_exported_purchase_tax_report': r'data/inputs/ภาษีซื้อ-ทรัพยเจิรญ67.csv', # r for non-ascii text
-    'csv_exported_sales_tax_report': r'data/inputs/ภาษีขาย-ทรัพย์เจริญ67.csv',
-    'excel_Withholding_tax_report': r'data/inputs/ขาย1-9-ทรัพย์.xlsx',
-    'excel_statement': 'data/inputs/20250319195146_merged_manual.xlsx',
-    'output_dir': 'data/output',
-    'matching_credit_days': 30,
-    'matching_sale_tolerance': 1000.0,
-    'matching_purchase_tolerance': 50.0,
-}
-
-# Define the output file paths based on CONFIG['output_dir']
-OUTPUT_FILES = {
-    'transaction_matches': os.path.join(CONFIG['output_dir'], 'รายงานสรุปผลการจับคู่รายการบัญชี.csv'),
-    'sale_status': os.path.join(CONFIG['output_dir'], 'รายงานสถานะใบกำกับภาษีขาย.csv'),
-    'purchase_status': os.path.join(CONFIG['output_dir'], 'รายงานสถานะใบกำกับภาษีซื้อ.csv'),
-    'withholding_status': os.path.join(CONFIG['output_dir'], 'รายงานสถานะรายการภาษีหักณที่จ่าย.csv'), 
-}
-
-
-EXPECTED_COLUMN_MAPPINGS = {
-    # report_name: {
-    #     column_name_en: 'column_name_th',
-    #     ...
-    # },
-    # ...
-    'purchase_tax_report': {
-        'order_number': 'ลำดับ',
-        'date_of_purchase_invoice': 'วัน/เดือน/ปี',
-        'purchase_invoice_tax_number': 'เลขที่ใบกำกับภาษี',
-        'purchase_invoice_id': 'เลขที่เอกสาร',
-        'company_name': 'ชื่อผู้ซื้อสินค้า/ผู้รับบริการ', 
-        'company_tax_id': 'เลขประจำตัวผู้เสียภาษี',
-        'product_value': 'มูลค่าสินค้า',
-        'vat': 'ภาษีมูลค่าเพิ่ม',
-        'total_amount': 'จำนวนเงิน'
-    },
-    'sale_tax_report': {
-        'order_number': 'ลำดับ',
-        'date_of_sale_invoice': 'วัน/เดือน/ปี',
-        'sale_invoice_tax_number': 'เลขที่ใบกำกับภาษี',
-        'company_name': 'ชื่อผู้ซื้อสินค้า/ผู้รับบริการ',
-        'company_tax_id': 'เลขประจำตัวผู้เสียภาษี',
-        'product_value': 'มูลค่าสินค้า',
-        'vat': 'ภาษีมูลค่าเพิ่ม',
-        'total_amount': 'จำนวนเงิน',
-        'withholding_tax': 'หัก 3%',
-        'net_amount': 'คงเหลือ',
-        'matched': 'จับคู่แล้ว', # Matched status mapping
-        'days_outstanding': 'จำนวนวันที่ค้างชำระ',
-    },
-    'withholding_tax_report': {
-        'paid_date': 'ว/ด/ป',
-        'company_name': 'ชื่อผู้หักภาษี',
-        'tax_id': 'เลขประจำตัวผู้เสียภาษี',
-        'amount': 'จำนวนเงิน',
-        'withholding_tax': 'หัก ณ ที่จ่าย',
-        'paid_amount': 'ยอดโอน',
-        'days_since_payment': 'จำนวนวันนับจากชำระ',
-        'matched': 'จับคู่แล้ว', # Matched status mapping
-    },
-     'statement': {
-        'datetime': 'วันที่และเวลา',
-        'amount': 'จำนวนเงิน',
-        'isDeposit': 'การฝากเงิน?',
-        'balance': 'ยอดคงเหลือ',
-        'page': 'หน้าที่'
-    },
-    'transaction_match_report': {
-         'ประเภทรายการ': 'ประเภทรายการ',
-         'วันที่ทำรายการ': 'วันที่ทำรายการ',
-         'จำนวนเงิน': 'จำนวนเงิน',
-         'บริษัท': 'บริษัท',
-         'รหัสประจำตัวผู้เสียภาษี': 'รหัสประจำตัวผู้เสียภาษี',
-         'เลขที่ใบกำกับภาษี': 'เลขที่ใบกำกับภาษี', # Use for matched invoice numbers
-         'วันที่จ่าย': 'วันที่จ่าย', # Use for matched withholding paid dates
-         'จำนวนเงินที่จับคู่': 'จำนวนเงินที่จับคู่',
-         'ส่วนต่าง': 'ส่วนต่าง',
-         'ประเภทการจับคู่': 'ประเภทการจับคู่' # Column indicating how the match was made (Sale, Withholding, Combination)
-    }
-}
 # Add mapping for the output transaction match report columns
 # This mapping is used internally by TransactionMatcher for report generation
 EXPECTED_COLUMN_MAPPINGS['transaction_match_report'] = TransactionMatcher.TRANSACTION_REPORT_COLS_MAP
 
 report_processor = None 
 transaction_matcher = None 
-
+#FIXME - move the main process to app.py and use the GUI class to run the app
 # init services eg: Gemini, EasyOcr, ...
 def initialize_services() -> None:
     try:
