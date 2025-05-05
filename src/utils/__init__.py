2550ae0a07c78f40ce8aa04d737c1c988bc2d0b4
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2025-05-05 20:37:29 +0700

    delete unuse color config

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,4 @@
 from .log_setup import initialize_logging, get_logger, get_run_id
 from .file_operations import FileManager
-from .color import fg, bg, style
 from .data_cleaner import DataFrameCleaner
 from .configs import CONFIG, OUTPUT_FILES, EXPECTED_COLUMN_MAPPINGS
